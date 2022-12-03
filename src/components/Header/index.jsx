ee3012a8459f09c20b3dc1a64b648e506f28145b
alexey-5 <107930630+alexey-5@users.noreply.github.com>
2022-12-03 11:00:30 +0300

    фин-2

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,7 +12,7 @@ function Header({openCart}) {
          </div>
          <ul className={styles.header__right}>
             <li onClick={openCart} className={styles.cart}>
-               <img width={18} height={18} src={require('../../img/cart18.svg').default} alt="cart" /><span>1205 руб.</span>
+               <img width={18} height={18} src={require('../../img/cart18.svg').default} alt="cart" /><span>1205 ₽ .</span>
             </li>
             <li>
                <img className={styles.header__right_like} width={20} height={20} src={require('../../img/like20.svg').default} alt="like20" />
